836883995f8df606a38d34c85f1da9579bcec773
sub77 <sub77@ymail.com>
2015-10-14 20:16:10 +0200

    update kernel patch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo bliss_matisse-eng
 add_lunch_combo bliss_matisse-userdebug
-sh device/samsung/matisse/patches/apply.sh
+#sh device/samsung/matisse/patches/apply.sh
+sh echo "####### APPLY PATCHES WITH BUILD SCRIPT !!!!"
 
