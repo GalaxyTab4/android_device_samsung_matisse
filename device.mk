22bfb5445beb3a6384b38362f0857dae8e494d0d
sub77 <sub77@ymail.com>
2015-10-28 11:06:39 +0100

    copy media_codecs_ffmpeg.xml from stagefright-plugins
    Change-Id: I48b6f88f3f42c691ba4d5a920dab0b592f95190a

--- device.mk
+++ device.mk
@@ -31,7 +31,8 @@ $(call inherit-product, vendor/samsung/matisse/matisse-vendor.mk)
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Doze
 PRODUCT_PACKAGES += \
