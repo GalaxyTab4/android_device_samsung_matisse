904f41ee399645d78a65bcdc72a3e7b082ef700d
morisbartyno <morisbartyno@gmail.com>
2015-11-29 20:56:08 +0500

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo omni_mmillet3gxx-userdebug
+add_lunch_combo omni_millet3gxx-userdebug
 add_lunch_combo omni_millet3g-eng
